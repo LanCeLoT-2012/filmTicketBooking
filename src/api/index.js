46d97b48c1765bed78bb7ccb94027b0fef58d4ed
KhanhLieu <khanhlieu1710@gmail.com>
2021-04-15 21:58:45 +0700

    Update server url

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const callApi = axios.create({
-	baseURL: "http://localhost:5000/api",
+	baseURL: "https://fanxine-be.herokuapp.com/api",
 	withCredentials: true,
 });
 
